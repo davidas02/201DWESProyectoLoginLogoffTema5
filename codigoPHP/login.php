80813f6904a7095d312970377c14cd877b681cbc
David Aparicio Sir <davidapariciosir@gmail.com>
2022-12-07 09:39:13 +0100

    07/12/2022 Añadir comentarios y cambio de ubicacion de los scripts de creacion de la base de datos en explotación Trabajo en clase

--- codigoPHP/login.php
+++ codigoPHP/login.php
@@ -5,7 +5,7 @@
  *  @since 07/12/2022
  */
 require_once '../core/221024libreriaValidacionFormularios.php';
-require_once '../conf/confDBPDODesarrollo.php';
+require_once '../conf/confDBPDOExplotacion.php';
 
 $entradaOk = true;
 //Array de respuestas para guardar las respuestas del formulario.
